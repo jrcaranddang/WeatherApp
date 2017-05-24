38b18dd2d6b2ec569c4f1b1fef41e2d779d40170
jrcaranddang <jrcaranddang@gmail.com>
2017-05-24 11:19:03 -0700

    windows branch

--- WeatherApp-backend/common/models/ww-users.js
+++ WeatherApp-backend/common/models/ww-users.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Wwusers) {
-
+    // Wwusers.observe('after save', function(ctx, next) {
+    //     if(ctx.isNewInstance === true) {
+    //         var instance = ctx.instance;
+    //         instance.createAccessToken(1209600000, function(err, res) {
+    //             if(err === null) ctx.instance["token"] = res.id;
+    //             next();
+    //         });
+    //     } else {
+    //         next();
+    //     }
+    // });
 };
